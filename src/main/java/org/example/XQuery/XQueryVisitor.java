398a963e4d342c265a4605ebfe78d80b231483bc
Hongstr <honghongon@gmail.com>
2024-02-24 14:30:30 -0800

    update queries

---
+++ src/main/java/org/example/XQuery/XQueryVisitor.java
@@ -0,0 +1,7 @@
+package org.example.XQuery;
+
+import org.example.parsers.XQueryBaseVisitor;
+
+public class XQueryVisitor extends XQueryBaseVisitor {
+
+}
---
+++ src/main/java/org/example/queries/BaseXQuery.java
@@ -0,0 +1,40 @@
+package org.example.queries;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public interface BaseXQuery {
+
+    List<Node> evaluate(List<Node> nodes) throws Exception;
+
+    default void getAllDescentNodes(List<Node> inputNodes, List<Node> result) {
+        for(Node n: inputNodes) {
+            result.add(n);
+            NodeList childNodes = n.getChildNodes();
+            List<Node> children = new ArrayList<>();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                children.add(childNodes.item(i));
+            }
+            getAllDescentNodes(children, result);
+        }
+    }
+    default Node makeText(Document doc, String text) {
+        return doc.createTextNode(text);
+    }
+
+    default Node makeElement(Document doc, String tagName, List<Node> list) {
+        Node node = doc.createElement(tagName);
+        for(Node n : list) {
+            if(Objects.nonNull(n)) {
+                node.appendChild(doc.importNode(n, true));
+            }
+        }
+        return node;
+    }
+
+}
